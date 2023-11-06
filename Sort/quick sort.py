4e2cabe28e80d970249801f52793efa940ac6803
zwoong <zwoong6867@gmail.com>
2023-11-06 23:39:51 +0900

    quick sort

---
+++ Sort/quick sort.py
@@ -0,0 +1,32 @@
+"""
+퀵 정렬 : 가장 많이 사용되는 정렬 알고리즘으로, 피벗이라는 개념이 사용된다.
+피벗 : 큰 숫자와 작은 숫자를 교환할 떄, 교환하기 위한 '기준'
+"""
+
+# 퀵 정렬 소스코드
+array =[5,7,9,0,3,1,6,2,4,8]
+
+def quick_sort(array, start, end):
+    if start >= end: # 원소가 1개인 경우 종료
+        return
+    pivot = start # pivot 은 첫 번째 원소
+    left = start + 1
+    right = end
+    
+    while left<=right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right:  #엇갈렸다면 작은 데이터와 피벗을 교체
+            array[right], array[pivot] = array[pivot], array[right]
+        else: #엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            array[left], array[right] = array[right], array[left]
+    #분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 다시 정렬 수행
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1, end)
+
+quick_sort(array, 0, len(array)-1)
+print(array)
